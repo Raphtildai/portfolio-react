474d0034c07d0d9b9db03162782f2ecdea1524fa
Raphael Tildai <raphaeltildai5@gmail.com>
2023-03-18 22:25:27 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Home from "./routes/home";
+import About from "./routes/about";
+import Certifications from "./routes/certifications";
+import Projects from "./routes/projects";
+import Contact from "./routes/contact";
+
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/certifications" element={<Certifications />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/certifications.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const certifications = () => {
+  return (
+    <div>certifications</div>
+  )
+}
+
+export default certifications;
